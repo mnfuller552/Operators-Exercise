b45656b041f2e9749ab9603aa55c2ecd70e59f7d
Matt Fuller <mnfuller552@gmail.com>
2023-03-05 18:42:19 -0500

    finished Operators Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+
+            Console.WriteLine("Please enter the radius of your circle");
+            var userInput = Console.ReadLine();
+
+
+            var radius = double.Parse(userInput);
+
+
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+
+
+        public static double AreaOfCircle(double radius)
+        {
+
+
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
+
         }
     }
 }
